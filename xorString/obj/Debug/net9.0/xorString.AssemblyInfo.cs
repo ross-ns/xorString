686955850dffd8a1cf5245cfc7834c34c80e5fe4
Ross <23499787+ross-ns@users.noreply.github.com>
2025-02-28 15:03:19 +0000

    Attempt at refactoring

--- xorString/obj/Debug/net9.0/xorString.AssemblyInfo.cs
+++ xorString/obj/Debug/net9.0/xorString.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xorString")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56168020fbfa843edbbf89ef38a18b415883e018")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ea6b128c5691d0cb882e2003d8f8e7a950b78e")]
 [assembly: System.Reflection.AssemblyProductAttribute("xorString")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xorString")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
