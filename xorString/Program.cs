9b562e9623cfbcbdee7f9fecc0e24c2b48c94953
Ross <23499787+ross-ns@users.noreply.github.com>
2025-04-30 16:38:32 +0100

    Bug fixes

--- xorString/Program.cs
+++ xorString/Program.cs
@@ -4,10 +4,6 @@ namespace xorString
 {
     internal class Program
     {
-        private static byte[] Input { get; set; }
-        private static byte[] Key { get; set; }
-        private static byte[] EncryptedString { get; set; }
-
         static void Main(string[] args)
         {
             Console.Write("Enter a string: ");
@@ -15,10 +11,10 @@ namespace xorString
             Console.Write("Enter a key: ");
             var key = Console.ReadLine();
             
-            EncryptString(input, key);
+            var encryptedString = EncryptString(input, key);
             
             Console.Write("Encrypted string: ");
-            foreach (var t in EncryptedString)
+            foreach (var t in encryptedString.encryptedString)
             {
                 Console.Write(t);
                 Console.Write(" ");
@@ -26,62 +22,54 @@ namespace xorString
             
             Console.WriteLine();
             
-            var decryptedString = DecryptString(EncryptedString, key);
-            Console.Write("Decrypted string: " + decryptedString);
+            var decryptedString = DecryptString(encryptedString.encryptedString, encryptedString.keyByte);
+            Console.Write("Decrypted string: ");
+            foreach (var t in decryptedString)
+            {
+                Console.Write(t);
+            }
         }
 
-        static void PadInputs(string input, string key)
+        static (byte[] encryptedString, byte[] keyByte) EncryptString(string input, string key)
         {
-            Console.Write("Input length: " + input.Length + " key length: " + key.Length);
+            // Check for differences in length of input and key
             if (input.Length < key.Length)
             {
-                input = input.PadLeft(key.Length - input.Length, 'x');
-                Input = Encoding.UTF8.GetBytes(input);
+                input = input.PadLeft(key.Length, '\u0000');
             }
             else if (input.Length > key.Length)
             {
-                key = key.PadLeft(input.Length - key.Length + 1, 'x');
-                Key = Encoding.UTF8.GetBytes(key);
+                key = key.PadLeft(input.Length, '\u0000');
             }
-            else if (input.Length == key.Length)
-            {
-                Input = Encoding.UTF8.GetBytes(input);
-                Key = Encoding.UTF8.GetBytes(key);
-            }
-            else
-            {
-                throw new Exception("Input lengths cannot be determined");
-            }
-        }
-
-        static void EncryptString(string input, string key)
-        {
-            // Pad inputs if required
-            PadInputs(input, key);
+            
+            // Convert input strings to byte arrays
+            var inputByte = Encoding.UTF8.GetBytes(input);
+            var keyByte = Encoding.UTF8.GetBytes(key);
             
             // XOR the string with the key
-            //var encryptedString = new byte[Input.Length];
+            var encryptedString = new byte[inputByte.Length];
             
-            for (int i = 0; i < Input.Length; i++)
+            for (int i = 0; i < inputByte.Length; i++)
             {
-                EncryptedString[i] = (byte)(Input[i] ^ Key[i]);
+                encryptedString[i] = (byte)(inputByte[i] ^ keyByte[i]);
             }
+            
+            return (encryptedString, keyByte);
         }
 
-        static string DecryptString(byte[] encryptedString, string key)
+        static string DecryptString(byte[] encryptedString, byte[] key)
         {
-            // Convert the key to a byte array
-            var keyByte = Encoding.UTF8.GetBytes(key);
-
             // XOR the encrypted string with the key
             var decryptedBytes = new byte[encryptedString.Length];
             for (int i = 0; i < encryptedString.Length; i++)
             {
-                decryptedBytes[i] = (byte)(encryptedString[i] ^ keyByte[i]);
+                decryptedBytes[i] = (byte)(encryptedString[i] ^ key[i]);
             }
             
             // Convert the decrypted byte array to a string
-            return Encoding.UTF8.GetString(decryptedBytes);
+            var decryptedString = Encoding.UTF8.GetString(decryptedBytes);
+
+            return decryptedString;
         }
     }
 }
